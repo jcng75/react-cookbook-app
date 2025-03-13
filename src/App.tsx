747124e2bceee9ab5f35ccefdd4bd961cfdceaee
Justin <justinchunng@gmail.com>
2025-03-12 22:59:37 -0400

    Updated App with html

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,40 @@ export default function App() {
 
   return (
     <>
-      <h1> Hello World! :D </h1>
+      <div>
+        <h1> Create a new Recipe </h1>
+        <form>
+          <label htmlFor="recipe">
+            Recipe Name:
+          </label>
+            <input id="recipe" type="text" name="name" />
+          <label htmlFor="ingredients">
+            Ingredients:
+          </label>
+            <textarea id="ingredients" name="ingredients" />
+          <button type="submit">Submit</button>
+          </form>
+        <h1> My Cool Cookbook Recipes </h1>
+        <label htmlFor="search"> Search </label>
+        <input id="search" type="text"></input>
+        <ul>
+          <li>
+            Recipe 1
+            <button> Info </button>
+            <button> Delete </button>
+            </li>
+          <li>
+            Recipe 2
+            <button> Info </button>
+            <button> Delete </button>
+            </li>
+          <li>
+            Recipe 3
+            <button> Info </button>
+            <button> Delete </button>
+            </li>
+        </ul>
+      </div>
     </>
   )
 }
